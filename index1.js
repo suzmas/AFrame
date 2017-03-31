89337dd0030ca44eb6cfe91710a4f0da72738487
Suzan Sucro <suzansucro@gmail.com>
2017-03-30 21:01:20 -0700

    Finish 3js box model component

--- index1.js
+++ index1.js
@@ -43,5 +43,38 @@ AFRAME.registerComponent('box', {
     height: {type: 'number', default: 1},
     depth: {type: 'number', default: 1},
     color: {type: 'color', default: '#AAA'}
+  },
+
+  init: function() {
+    var data = this.data;
+    var el = this.el;
+
+    //create geometry
+    this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
+    //create material
+    this.material = new THREE.Mesh(this.geometry, this.material);
+    // set mesh on entity
+    el.setObject3D('mesh', this.mesh);
+  },
+
+  update: function(oldData) {
+    var data = this.data;
+    var el = this.el;
+
+    if (Object.keys(oldData).length === 0) { return; }
+
+    if (data.width !== oldData.width ||
+        data.height !== oldData.height ||
+        data.depth !== oldData.depth) {
+          el.getObject3D('mesh').geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
+      }
+
+    if (data.color !== oldData.color) {
+      el.getObject3D('mesh').material.color = data.color;
+    }
+  },
+
+  remove: function() {
+    this.el.removeObject3D('mesh');
   }
 });
