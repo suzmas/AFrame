2ce3b94468e9bded5b53b3e793fc3ac38d57a226
Suzan Sucro <suzansucro@gmail.com>
2017-04-04 16:38:48 -0700

    Finish v1 of walk component

--- components/walk.js
+++ components/walk.js
@@ -1,17 +1,30 @@
-// follow compononent
+// walk component
 AFRAME.registerComponent('walk', {
   schema: {
-    speed: {type: 'number'},
+    walking: {default: true},
   },
   init: function() {
     this.directionVec3 = new THREE.Vector3();
+    this.el.sceneEl.addEventListener('click', function(){
+      var camEl = document.querySelector('a-camera');
+      var walking = camEl.getAttribute('walk').walking;
+      camEl.setAttribute('walk', 'walking', !walking);
+    });
   },
 
   tick: function(time) {
-    var directionVec3 = this.directionVec3;
 
-    var distance = 1;
-    this.el.object3D.translateZ( - distance );
+    if (this.data.walking) {
+      console.log("we are walking");
+      var directionVec3 = this.directionVec3;
 
+      var distance = .05;
+      this.el.object3D.translateZ( -distance );
+    }
   }
 });
+
+// NEED TO: //
+// stop user from moving off the ground
+// potentially add walk control target to top left of screen instead of entire
+// scene click listener
